7ded34152e58cdfec89379f159fe261f80ea2d56
MelonGO <cjwone1@163.com>
2017-02-23 15:43:49 +0800

    first commit
    init

---
+++ src/main/java/com/pet/beans/ConstantsInitializer.java
@@ -0,0 +1,19 @@
+package com.pet.beans;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.CharacterEncodingFilter;
+
+@Configuration
+public class ConstantsInitializer {
+	@Bean
+	public FilterRegistrationBean characterEncodingFilter() {
+		FilterRegistrationBean registrationBean = new FilterRegistrationBean();
+	    CharacterEncodingFilter filter = new CharacterEncodingFilter();
+	    filter.setEncoding("UTF-8");
+	    filter.setForceEncoding(true);
+	    registrationBean.setFilter(filter);
+	    return registrationBean;
+	}
+}
