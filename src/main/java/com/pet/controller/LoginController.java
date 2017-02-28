1cd9e20801f72be7742d0bc67d8703092881c61b
王宁 <841822513@qq.com>
2017-02-28 14:47:14 +0800

    简易的登录界面
    增加了简易的登录界面

---
+++ src/main/java/com/pet/controller/LoginController.java
@@ -0,0 +1,15 @@
+package com.pet.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class LoginController {
+	
+	@RequestMapping(path = {"/login"})
+	public String login(){
+		return "login";
+	}
+
+}
+
