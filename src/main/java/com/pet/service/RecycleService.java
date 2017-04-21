7b6b573b6dc948d67d03ec4d30245e5e2020230e
王宁 <841822513@qq.com>
2017-04-21 17:06:23 +0800

    完善送回宠物的功能
    11

--- src/main/java/com/pet/service/RecycleService.java
+++ src/main/java/com/pet/service/RecycleService.java
@@ -1,30 +1,21 @@
 package com.pet.service;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import com.pet.dao.AdoptionDAO;
 import com.pet.dao.RecycleDAO;
-import com.pet.model.Adoption;
-import com.pet.model.ReceivingInfo;
 import com.pet.model.Recycle;
 
 @Service
-public class RecyleService {
-	
-	@Autowired
-	private AdoptionDAO adoptionDao;
+public class RecycleService {
 	
 	@Autowired
 	private RecycleDAO recycleDao;
 	
-	public Map<String, Object> addAdoption(int petId, int userId, int isSponsorship, double money, String reason, String address,String phone) {
+	public Map<String, Object> addRecycle(int petId, int userId, int isSponsorship, double money, String reason, String address,String phone) {
 		Map<String, Object> msgMap = new HashMap<>();
 		Recycle recycle=new Recycle();
-		
+		recycle.setState(0);
 		recycle.setPetId(petId);
 		recycle.setUserId(userId);
 		recycle.setMoney(money);
--- src/main/resources/static/scripts/recycle.js
+++ src/main/resources/static/scripts/recycle.js
@@ -1,6 +1,57 @@
 $(function(){
-	$("#btnRecycle").click(function(id,obj){  
-		
-		
+	
+	$('.btnRecycle').click(function (event) {  
+		var trs=$("table#table1 tbody tr");
+		var tr=$(this).closest("tr");
+        var index=trs.index(tr);
+        var td=tr.eq(0).find("td")
+        var type=td.eq(0).text();
+        var age=td.eq(1).text();
+        var name=td.eq(2).text();
+        var sex=td.eq(3).text();
+        var price=td.eq(4).text();
+        var adoptTime=td.eq(5).text().substring(0,10);
+        var petID=td.eq(6).text();
+       
+        var modal=$("#recycleModal");
+        modal.find('.modal-body #recyclePetID').val(petID) ;
+        modal.find('.modal-body #recyclePetName').val(name) ;
+        modal.find('.modal-body #recyclePetAge').val(age) ;
+        modal.find('.modal-body #recyclePetType').val(type) ;
+        modal.find('.modal-body #recyclePetAdoptTime').val(adoptTime) ;
+	}) ;
+	
+	
+	$("#recycleConfirm").click(function(){  
+		var reason=$("#recycleReason").val();
+		var pid=$("#recyclePetID").val();
+		var address=$("#recycleAddress").val();
+		var phone=$("#recyclePhone").val();
+		var money=$("#recycleMoney").val();
+		var sponsorship=$("#recycleSponsorship").val();
+		$.getJSON("recycle.do",{"reason":reason,"pid":pid,"address":address,"phone":phone,"money":money,"sponsorship":sponsorship},function(d){
+			
+			if(d=="0"){
+			   alert("请登录");
+		   }
+			if(d=="1"){
+			   alert("无权限，请尝试重新登录");
+		   }
+			if(d=="2"){
+
+			   setTimeout(function(){  
+				   window.location.reload();
+			   },2000);
+		   }
+		});		
 	});
+	
+	
+	$("#recycleSponsorship").click(function(){  
+		var modal=$("#recycleModal");
+		 modal.find('.modal-body #recycleSponsorship').val("") ;
+
+	});
+	
+	
 });
