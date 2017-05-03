e806a01a6c4387e89d9cce62802f9ad1f5cbacf8
MSI2015 <MSI2015@172.19.162.44>
2017-05-03 16:44:23 +0800

    comment block

---
+++ src/main/java/com/pet/model/Comment.java
@@ -0,0 +1,42 @@
+package com.pet.model;
+
+public class Comment extends Base{
+	private int petID;
+	private String username;
+	private String content;
+	private int fatherCommentID;
+	private int like;
+	
+	public int getCommentID() {
+		return petID;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public String getContent() {
+		return content;
+	}
+	public int getFatherCommentID() {
+		return fatherCommentID;
+	}
+	public int getLike() {
+		return like;
+	}
+	public void setCommentID(int commentID) {
+		this.petID = commentID;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public void setFatherCommentID(int fatherCommentID) {
+		this.fatherCommentID = fatherCommentID;
+	}
+	public void setLike(int like) {
+		this.like = like;
+	}
+	
+	
+}
